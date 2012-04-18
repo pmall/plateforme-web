562def3257bbe4c21c382ce7aa980294b92ee423
Pierre <pierre.mallinjoud@gmail.com>
2012-04-18 15:32:09 +0200

    push de la config

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+# Repertoire de l'application
+$config['dir_app'] = 'plateforme2';
+
+# Fichiers contenant les celfiles
+$config['celdir'] = '/home/pierre/workspace/celdir';
+
+# Base de données
+$config['db_name'] = 'elexir2';
+$config['db_host'] = 'localhost';
+$config['db_user'] = 'pierre';
+$config['db_pass'] = 'ysbag';
+
+?>
